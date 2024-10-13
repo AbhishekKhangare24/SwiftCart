b823bdca1924e93dc55f652b121070ad0d74c5ec
Abhishek Khangare <abhi@gmail.com>
2024-10-13 14:35:02 +0530

    updated logo, App component, star rating, cart wrapper, and skeleton components with new SVG animations, styles, and layout changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,266 @@ function App() {
     dispatch(checkAuth(token));
   }, [dispatch]);
 
-  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+  if (isLoading)
+    return (
+      <Skeleton className="w-full h-screen flex justify-center">
+        <div className="flex justify-center flex-col">
+          <div className="flex justify-center">
+            {/* green */}
+            <svg
+              className="h-32"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 200"
+            >
+              <rect
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="25"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.4"
+                ></animate>
+              </rect>
+              <rect
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="85"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.2"
+                ></animate>
+              </rect>
+              <rect
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="145"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="0"
+                ></animate>
+              </rect>
+            </svg>
+            {/* <svg
+              className="h-32"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 200"
+            >
+              <circle
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                r="15"
+                cx="40"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.4"
+                ></animate>
+              </circle>
+              <circle
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                r="15"
+                cx="100"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.2"
+                ></animate>
+              </circle>
+              <circle
+                fill="#2BFF6E"
+                stroke="#2BFF6E"
+                stroke-width="15"
+                r="15"
+                cx="160"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="0"
+                ></animate>
+              </circle>
+            </svg> */}
+            {/* black */}
+            {/* <svg
+              className="h-32 opacity-20"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 200"
+            >
+              <circle
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                r="15"
+                cx="40"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.4"
+                ></animate>
+              </circle>
+              <circle
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                r="15"
+                cx="100"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.2"
+                ></animate>
+              </circle>
+              <circle
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                r="15"
+                cx="160"
+                cy="100"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="0"
+                ></animate>
+              </circle>
+            </svg> */}
+            {/* <svg
+              className="h-32 opacity-40"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 200"
+            >
+              <rect
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="25"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.4"
+                ></animate>
+              </rect>
+              <rect
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="85"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="-.2"
+                ></animate>
+              </rect>
+              <rect
+                fill="#05200E"
+                stroke="#05200E"
+                stroke-width="15"
+                width="30"
+                height="30"
+                x="145"
+                y="85"
+              >
+                <animate
+                  attributeName="opacity"
+                  calcMode="spline"
+                  dur="2"
+                  values="1;0;1;"
+                  keySplines=".5 0 .5 1;.5 0 .5 1"
+                  repeatCount="indefinite"
+                  begin="0"
+                ></animate>
+              </rect>
+            </svg> */}
+          </div>
+          <p className="mx-auto px-20 text-center">
+            This hosting is free and may take up to 30 seconds
+          </p>
+        </div>
+      </Skeleton>
+    );
 
   console.log(isLoading, user);
 
--- client/src/components/ui/skeleton.jsx
+++ client/src/components/ui/skeleton.jsx
@@ -1,10 +1,12 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}) {
-  return (<div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />);
+function Skeleton({ className, ...props }) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-none bg-muted", className)}
+      {...props}
+    />
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
