6406e64c75292ca211eb4154160c862d28b660fc
Abhishek Khangare <abhi@gmail.com>
2024-10-13 17:58:08 +0530

    added useState hook to MenuItems and AdminSideBar components, updated className for menu items, and modified onClick handlers to set state and navigate

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -4,7 +4,7 @@ import {
   LayoutDashboard,
   ShoppingBasket,
 } from "lucide-react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
@@ -29,7 +29,8 @@ const adminSidebarMenuItems = [
   },
 ];
 
-function MenuItems({ setOpen }) {
+function MenuItems({ setOpen, data1 }) {
+  const [data, setData] = useState("");
   const navigate = useNavigate();
 
   return (
@@ -38,10 +39,11 @@ function MenuItems({ setOpen }) {
         <div
           key={menuItem.id}
           onClick={() => {
+            setData(menuItem.id);
             navigate(menuItem.path);
             setOpen ? setOpen(false) : null;
           }}
-          className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
+          className={`flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-green-50 hover:text-green-600`}
         >
           {menuItem.icon}
           <span>{menuItem.label}</span>
@@ -52,6 +54,7 @@ function MenuItems({ setOpen }) {
 }
 
 function AdminSideBar({ open, setOpen }) {
+  const [data, setData] = useState("");
   const navigate = useNavigate();
 
   return (
@@ -71,7 +74,10 @@ function AdminSideBar({ open, setOpen }) {
       </Sheet>
       <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
         <div
-          onClick={() => navigate("/admin/dashboard")}
+          onClick={() => {
+            setData("dashboard");
+            navigate("/admin/dashboard");
+          }}
           className="flex cursor-pointer items-center gap-2"
         >
           <ChartNoAxesCombined size={30} />
